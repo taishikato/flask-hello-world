f9141bb9895588a69b14212a7671609d90d5d464
taishikato <taishi.k0903@gmail.com>
2023-03-12 22:24:49 -0700

    update app

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
+from flask import Flask,request
+import whisper
+import os
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route("/download", methods=['POST'])
+def download():
+  file = request.files['file']
+  print(file)
+
+  if file.filename != None:
+    print(file.filename)
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    model = whisper.load_model("base")
+    result = model.transcribe('./' + filename, verbose=True, language='en')
+    # print(result)
+    os.remove('./' + filename)
+    # model.cpu()
+    # del model
+
+    # requests.post("http://localhost:3000/api/save-transcriptions-test", data=json.dumps(result))
+
+    return json.dumps(result)
+
+  return 'hello'
